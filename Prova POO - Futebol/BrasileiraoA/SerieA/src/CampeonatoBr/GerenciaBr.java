b694312faab48356ada6742423784d5c0ca096a3
Roberto Santana <88199833+RobertoSantanna@users.noreply.github.com>
2023-04-16 11:20:23 -0300

    Add files via upload

---
+++ Prova POO - Futebol/BrasileiraoA/SerieA/src/CampeonatoBr/GerenciaBr.java
@@ -0,0 +1,17 @@
+package CampeonatoBr;
+
+/**
+ *
+ * @author Marco Santana
+ * @author Roberto Santana
+ */
+
+public class GerenciaBr{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Brasileirao plm = new Brasileirao(5); //Ver se funciona só com o new Brasileirao(5)
+    }
+}
